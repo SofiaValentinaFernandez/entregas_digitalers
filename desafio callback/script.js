709c5118be9eecc2efbf0224487cb5f86ac2a474
SofiaValentinaFernandez <valentinafernandez23313@gmail.com>
2022-08-04 17:52:17 -0300

    crear repo en git

---
+++ desafio callback/script.js
@@ -0,0 +1,8 @@
+/* CONSIGNA
+Cree una función llamada  function gastoConIva() que acepte un número y devuelva ese número más el 21% que el valor del IVA.
+Luego crea una function map() que tome dos entradas o parámetros:
+un array de como valores números. 
+una función callback: esta función se aplica a cada elemento del array (dentro de la función map().
+Haga que su function map() devuelva una nueva matriz llena de números que son el resultado de usar la función callback en cada elemento de la matriz de entrada.
+*/
+
