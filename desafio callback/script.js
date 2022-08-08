e9921f91d7c8dbb50a81ca3912dc528c8fbf50cf
SofiaValentinaFernandez <valentinafernandez23313@gmail.com>
2022-08-08 12:32:59 -0300

    gasto con IVA

--- desafio callback/script.js
+++ desafio callback/script.js
@@ -6,3 +6,14 @@ una función callback: esta función se aplica a cada elemento del array (dentro
 Haga que su function map() devuelva una nueva matriz llena de números que son el resultado de usar la función callback en cada elemento de la matriz de entrada.
 */
 
+// gasto con IVA
+console.log("Gasto con IVA")
+function gastoConIva(){
+    let numero= 1000;
+    let conIva = numero+(numero*(21/100));
+    console.log('El total con iva es: ' + conIva)
+}
+gastoConIva(); 
+
+console.log("__________________________")
+
