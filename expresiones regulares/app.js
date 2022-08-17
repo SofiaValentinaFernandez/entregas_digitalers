dcdb68640d3f31c2ba06efa4da7082a253d5fd2f
SofiaValentinaFernandez <valentinafernandez23313@gmail.com>
2022-08-16 21:57:27 -0300

    creacion de carpeta desafio 2

---
+++ expresiones regulares/app.js
@@ -0,0 +1,12 @@
+/* CONSIGNA
+
+Tenemos un cliente que nos está pidiendo que el sistema que estamos desarrollando tenga una 
+contraseña segura pero con las siguientes característica, que esta debe contener: 
+
+1) Que tenga una longitud de 12 caracteres mínimo.
+2) Debe contener al menos 1 letra en mayúscula.
+3) Debe contener al menos 1 letra en minúscula.
+4) No puede contener espacios vacíos.
+
+*/
+
